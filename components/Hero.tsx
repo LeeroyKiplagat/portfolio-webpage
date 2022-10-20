a9b80d32320b383ce1ec409d8fd19cb3a355c9c3
Leeroy <leeroykiplagat@gmail.com>
2022-10-20 12:43:39 +0300

    Hello

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ export default function Hero({pageInfo}: Props) {
       <BackgroundCircles />
       <img
         className="relative rounded-full h-32 w-32  mx-auto object-cover"
-        src={urlFor(pageInfo?.heroImage).url( )}
+        src={urlFor(pageInfo?.heroImage).url()}
         alt=""
       />
 
